01ac65a706c32d985c6518a848aef484b8e9e558
Ron Coleman <zendrumcorps@yahoo.com>
2021-04-29 15:58:37 -0400

    Header finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Particles from 'react-particles-js'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
-  return <Navbar />
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 40,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: 'cirle',
+              strike: {
+                width: 6,
+                color: '#6cc95e',
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  )
 }
 
 export default App
