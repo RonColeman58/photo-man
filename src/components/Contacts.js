05d7377038dc862bc3b50497dd4ba5105aba543d
Ron Coleman <zendrumcorps@yahoo.com>
2021-05-26 10:06:27 -0400

    Contacts component Completed

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,36 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import emailjs from 'emailjs-com'
+import { useForm } from 'react-hook-form'
 
 const Contacts = () => {
-  const serviceID = 'serviceID'
-  const templateID = 'templateID'
+  const [successMessage, setSucessMessage] = useState('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const serviceID = 'service_ID'
+  const templateID = 'template_ID'
   const userID = 'user_FaX4yqQXiWfj8ZfmVxho1'
 
-  const sendEmail = (e) => {
-    e.preventDefault()
-
-    emailjs.sendForm(serviceID, templateID, e.target, userID).then(
-      (result) => {
-        console.log(result.text)
+  const onSubmit = (data, r) => {
+    sendEmail(
+      serviceID,
+      templateID,
+      {
+        name: data.name,
+        phone: data.phone,
+        email: data.email,
+        subject: data.subject,
+        description: data.description,
       },
-      (error) => {
-        console.log(error.text)
-      }
+      userID
     )
+    r.target.reset()
+  }
+
+  const sendEmail = (serviceID, templateID, variables, userID) => {
+    emailjs
+      .send(serviceID, templateID, variables, userID)
+      .then(() => {
+        setSucessMessage(
+          "Your form has been sent! I'll contact you as soon as posible."
+        )
+      })
+      .catch((err) => console.error(`somethings gone wrong ${err}`))
   }
 
   return (
-    <div className="contacts">
+    <div id="contacts" className="contacts">
       <div className="text-center">
         <h1>contact me</h1>
         <p>
           Please fill out the form. Tell me about the event you would like me to
           capture.
         </p>
+        <span className="success-message">{successMessage}</span>
       </div>
 
       <div className="container">
-        <form onSubmit={sendEmail}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row">
             <div className="col-md-6 col-xs-12">
               {/* Name Input */}
@@ -40,9 +63,20 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Name"
                   name="name"
+                  aria-invalid={errors.name ? 'true' : 'false'}
+                  {...register('name', {
+                    required: 'Please enter your name',
+                    maxLength: {
+                      value: 20,
+                      message: 'Please enter a name fewer than 20 characters',
+                    },
+                  })}
                 />
                 <div className="line"></div>
               </div>
+              <span className="error-message">
+                {errors.name && errors.name.message}
+              </span>
               {/* Phone Input */}
               <div className="text-center">
                 <input
@@ -50,9 +84,16 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Phone Number"
                   name="phone"
+                  aria-invalid={errors.phone ? 'true' : 'false'}
+                  {...register('phone', {
+                    required: 'Please enter a phone number',
+                  })}
                 />
                 <div className="line"></div>
               </div>
+              <spam className="error-message">
+                {errors.phone && errors.phone.message}
+              </spam>
               {/* Email Input */}
               <div className="text-center">
                 <input
@@ -60,9 +101,20 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Email"
                   name="email"
+                  aria-invalid={errors.email ? 'true' : 'false'}
+                  {...register('email', {
+                    required: 'Please provide an email',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Invalid Email',
+                    },
+                  })}
                 />
                 <div className="line"></div>
               </div>
+              <span className="error-message">
+                {errors.email && errors.email.message}
+              </span>
               {/* Subject Input */}
               <div className="text-center">
                 <input
@@ -70,9 +122,16 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Subject"
                   name="subject"
+                  aria-invalid={errors.subject ? 'true' : 'false'}
+                  {...register('subject', {
+                    required: 'Oops, you forgot to include a subject',
+                  })}
                 />
                 <div className="line"></div>
-              </div>{' '}
+              </div>
+              <span className="error-message">
+                {errors.subject && errors.subject.message}
+              </span>
             </div>
             <div className="col-md-6 col-xs-12">
               {/* Description */}
@@ -80,10 +139,17 @@ const Contacts = () => {
                 <textarea
                   type="text"
                   className="form-control"
-                  placeholder="Give me a short description"
-                  name="email"
+                  placeholder="Give me a short description of your needs"
+                  name="drscription"
+                  aria-invalid={errors.description ? 'true' : 'false'}
+                  {...register('description', {
+                    required: 'Please desrcibe your project',
+                  })}
                 ></textarea>
                 <div className="line"></div>
+                <span className="error-message">
+                  {errors.description && errors.description.message}
+                </span>
               </div>
               <button className="btn-main-offer contact-btn" type="submit">
                 Send Email
